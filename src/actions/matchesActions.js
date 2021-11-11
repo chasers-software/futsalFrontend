b1596454f186a623951c717a6894c6eac0730919
Saujan Tiwari <saujan.tiwari@gmail.com>
2021-11-11 08:50:48 +0545

    filtering of matches done
    filtering of matches done

--- src/actions/matchesActions.js
+++ src/actions/matchesActions.js
@@ -1,27 +1,26 @@
-import axios from 'axios'
+import axios from "axios";
 import {
-    MATCHES_LIST_REQUEST,
-    MATCHES_LIST_SUCCESS,
-    MATCHES_LIST_FAIL
-} from '../constants/matchesConstants'
+  MATCHES_LIST_REQUEST,
+  MATCHES_LIST_SUCCESS,
+  MATCHES_LIST_FAIL,
+} from "../constants/matchesConstants";
 
-export const listMatches = () => async (dispatch) => {
-    try {
-        dispatch({
-            type: MATCHES_LIST_REQUEST,
-        })
+export const listMatches = (filter) => async (dispatch) => {
+  try {
+    dispatch({
+      type: MATCHES_LIST_REQUEST,
+    });
 
-        const { data } = await axios.get("/matches")
-        
-        dispatch({
-            type: MATCHES_LIST_SUCCESS,
-            payload: data.matches
-        })
-        
-    } catch (error) {
-        dispatch({
-            type: MATCHES_LIST_FAIL,
-            payload: error.response.data.msg
-        })
-    }
-}
+    const { data } = await axios.get("/matches", { params: filter });
+
+    dispatch({
+      type: MATCHES_LIST_SUCCESS,
+      payload: data.matches,
+    });
+  } catch (error) {
+    dispatch({
+      type: MATCHES_LIST_FAIL,
+      payload: error.response.data.msg,
+    });
+  }
+};
---
+++ src/dummyFilter.js
@@ -0,0 +1,54 @@
+const futsals = [
+  "G4 Futsal",
+  "Samakhusi Futsal",
+  "Monastry Futsal",
+  "Gongabu Futsal",
+];
+
+const dates = ["2021-11-10", "2021-11-11", "2021-11-12"];
+
+const startTimes = [
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+];
+const endTimes = [
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+];
+
+const prices = ["1000", "1200", "1500", "2000"];
+
+const statuses = ["open", "booked"];
+
+module.exports = {
+  futsals,
+  dates,
+  startTimes,
+  endTimes,
+  prices,
+  statuses,
+};
